5171a1f8d99dff0c134a7499058bfbbd99efed1d
Simran Thind <s2thind@cpu24.student.math.uwaterloo.ca>
2023-02-22 19:16:19 -0500

    edit parent to child message example

--- parent-to-child-message.c
+++ parent-to-child-message.c
@@ -8,7 +8,7 @@
 struct message {
     long message_type;
     int message_data;
-}
+};
 
 int main(int argc, char** argv) {
     int message_qid = msgget(IPC_PRIVATE, 0666 | IPC_CREAT);
@@ -21,7 +21,7 @@ int main(int argc, char** argv) {
     } else if (pid == 0) {
         // If the pid == 0, then the process we're in is the child
         struct message M2;
-        msgrcv(message_qid, &M2, sizeof(struct message), 42, 0);
+        msgrcv(message_qid, &M2, sizeof(int), 42, 0);
         printf("Received the message: %d!\n", M2.message_data);
         msgctl(message_qid, IPC_RMID, NULL);
     } else {
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
         struct message M1;
         M1.message_type = 42;
         M1.message_data = 252;
-        msgsnd(message_qid, &M1, sizeof(struct message), 0);
+        msgsnd(message_qid, &M1, sizeof(int), 0);
     }
     return 0;
 }
