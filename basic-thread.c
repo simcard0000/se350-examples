18fc728d13da43b75d40a43b2b2c6e75a60493b4
Simran Thind <simran.thind@outlook.com>
2023-02-23 18:41:15 -0500

    Create basic-thread.c

---
+++ basic-thread.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+void* run(void* argument) {
+    char* a = (char*) argument;
+    printf("Provided argument: %s.\n", a);
+    int* return_value = malloc(sizeof(int));
+    *return_value = 99;
+    pthread_exit(return_value);
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("Invalid arguments.\n");
+        return -1;
+    }
+    pthread_t = t;
+    void* vr;
+
+    pthread_create(&t, NULL, run, argv[1]);
+    pthread_join(t, &vr);
+    int* r = (int*) vr;
+    printf("The other thread returned the value: %d.\n", *r);
+    free(vr);
+    pthread_exit(0);
+    return 0;
+}
