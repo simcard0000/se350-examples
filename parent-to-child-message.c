110825f31250431b3cffc7bd54cade26f9c112ec
Simran Thind <simran.thind@outlook.com>
2023-02-22 19:09:19 -0500

    initialize parent-to-child-message.c

---
+++ parent-to-child-message.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/msg.h>
+#include <unistd.h>
+
+struct message {
+    long message_type;
+    int message_data;
+}
+
+int main(int argc, char** argv) {
+    int message_qid = msgget(IPC_PRIVATE, 0666 | IPC_CREAT);
+
+    int pid = fork();
+
+    if (pid < 0) {
+        // If the pid < 0, then an error has occured
+        fprintf(stderr, "Forking a child process has failed.");
+    } else if (pid == 0) {
+        // If the pid == 0, then the process we're in is the child
+        struct message M2;
+        msgrcv(message_qid, &M2, sizeof(struct message), 42, 0);
+        printf("Received the message: %d!\n", M2.message_data);
+        msgctl(message_qid, IPC_RMID, NULL);
+    } else {
+        // If the pid > 0, then the process we're in is the parent
+        struct message M1;
+        M1.message_type = 42;
+        M1.message_data = 252;
+        msgsnd(message_qid, &M1, sizeof(struct message), 0);
+    }
+    return 0;
+}
