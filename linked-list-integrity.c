c409cffd3dc06181ad9f5fb225527ce45329eb6e
Simran Thind <simran.thind@outlook.com>
2023-02-23 21:29:45 -0500

    add param var name

--- linked-list-integrity.c
+++ linked-list-integrity.c
@@ -63,7 +63,7 @@ single_list_t data_list;
 
 // The below code is a personal example:
 
-void* task1(void *) {
+void* task1(void *param) {
     int value1 = 0;
     int value2 = 5;
     int value3 = 7;
@@ -72,7 +72,7 @@ void* task1(void *) {
     push_front(&data_list, &value3);
 }
 
-void* task2(void *) {
+void* task2(void *param) {
     int value4 = 9;
     int value5 = 34;
     int value6 = 222;
@@ -83,7 +83,7 @@ void* task2(void *) {
     push_front(&data_list, &value6);
 }
 
-void* task3(void *) {
+void* task3(void *param) {
     pop_front(&data_list);
     pop_front(&data_list);
     pop_front(&data_list);
